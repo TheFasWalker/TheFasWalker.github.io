f3b6880576a5909007a6e36b3f476065ae5a3537
Fast <krinickij.v.a@gmail.com>
2024-03-31 09:19:50 +0300

    fix not used connections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import './App.css';
-import { Routes,Route,Link, BrowserRouter } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { OperationsPage } from './pages/OperationsPage';
-import { OperationPage } from './pages/OperationPage';
-import { ProfilePage } from './pages/ProfilePage';
 import { Navigation } from './navigation/Navigation';
 function App() {
 
